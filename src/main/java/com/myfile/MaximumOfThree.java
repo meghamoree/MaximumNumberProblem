547491c5abfa819590560108cfa9aaaf74e9db90
meghamoree <meghamoree@gmail.com>
2020-05-07 15:57:46 +0530

    Add.Constructor with 3 inputs as parameter.

--- src/main/java/com/myfile/MaximumOfThree.java
+++ src/main/java/com/myfile/MaximumOfThree.java
@@ -7,6 +7,13 @@ package com.myfile;
         public MaximumOfThree(){
 
         }
+        //Parameterised Constructor
+        public MaximumOfThree(E firstInput, E secondInput, E thirdInput){
+            this.firstInput=firstInput;
+            this.secondInput=secondInput;
+            this.thirdInput=thirdInput;
+        }
+
         //Generic function that accepts all the data types
             public <E extends Comparable > E findMaximum(E firstInput, E secondInput, E thirdInput) {
                 //<< >> != operators does not work in generics thus using .compare
